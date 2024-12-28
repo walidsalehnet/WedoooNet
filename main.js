3259299194445aa46624e75201ce9c9b26eabfc8
walidsalehnet <salehwalid078@gmail.com>
2024-12-29 01:14:04 +0200

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyDhd3l_1q-gNEeihQQY-5fpRvV9vCZgMYA",
     authDomain: "wedoo7.firebaseapp.com",
     projectId: "wedoo7",
@@ -9,8 +17,11 @@ const firebaseConfig = {
     appId: "1:176039023490:web:693f81466bd00a8767f59b",
     measurementId: "G-Y3TLQ3CKYJ"
   };
-firebase.initializeApp(firebaseConfig);
 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 // Login function
 function login() {
     const phone = document.getElementById('phone').value;
